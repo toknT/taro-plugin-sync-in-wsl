b88581dcccefa17c66c8ff213375eff0ced25a7a
toknsiT <toknsiT@outlook.com>
2020-12-07 14:19:15 +0800

    update: rsync command option to -az

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,15 @@
 import { exec, which, echo } from 'shelljs'
 export default (ctx: any, options: any) => {
-    ctx.onBuildFinish(() => {
-        let platform = ctx.runOpts.platform
-        let target = options.dists[platform]
-        if (target) {
-            if (which('sync')) {
-                let execShell = `rsync -r ${ctx.paths.outputPath} ${target}`
-                exec(execShell)
-            } else {
-                echo('sync not found')
-            }
-        }
-    })
+  ctx.onBuildFinish(() => {
+    let platform = ctx.runOpts.platform
+    let target = options.dists[platform]
+    if (target) {
+      if (which('sync')) {
+        let execShell = `rsync -av ${ctx.paths.outputPath} ${target}`
+        exec(execShell)
+      } else {
+        echo('sync not found')
+      }
+    }
+  })
 }
